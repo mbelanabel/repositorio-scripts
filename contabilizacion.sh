26d94df8cbd9b17c642f34f3b97cb97770b8dbcb
mbelanabel <mbelanabel@gmail.com>
2017-10-18 13:49:21 +0200

    Add files via upload

---
+++ contabilizacion.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+unset usuario
+unset cuent
+sudo cat /etc/shadow > .shadow
+usuario=( "Sin contraseña ::" "Cuenta desactivada" "Contraseña de grupo" "Expirada")
+cuent=(0 0 0 0)
+while read var
+do
+ #Comprobamos  contraseña de usuario
+ if [[ $( echo $var | cut -f2 -d":" ) = "!" ]]
+ then
+	cuent[1]=`expr ${cuent[1]} + 1`
+ else
+   if [[ $( echo $var | cut -f2 -d":" ) = "*" ]]
+   then
+        cuent[2]=`expr ${cuent[2]} + 1`
+   else
+       if [[ $( echo $var | cut -f2 -d":" ) = "!!" ]]
+       then
+          cuent[3]=`expr ${cuent[3]} + 1`
+       else
+          cuent[0]=`expr ${cuent[0]} + 1`
+       fi
+   fi
+ fi
+done < .shadow
+for ((i=0; i<${#usuario[@]};i++))
+do 
+  echo ${usuario[$i]} ${cuent[$i]}
+done
