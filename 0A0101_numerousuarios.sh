aae1d401960eff86a532afebecef422eb25389b2
Ana Isabel Miguel Bello <mbelanabel@gmail.com>
2023-10-30 10:43:33 +0100

    Update 0A0101_numerousuarios.sh

--- 0A0101_numerousuarios.sh
+++ 0A0101_numerousuarios.sh
@@ -3,5 +3,6 @@
 #Objetivo funcional: calcular el numero de usuarios conectados
 #Objetivo didactico: primeros scripts
 #Key words: pipe, cut, sort, uniq, wc, comillas inversas
+
 #!/bin/bash
 who -a | cut -f1 -d" " | sort | uniq | echo "Hay `wc -w` usuarios conectados"
