c16968e9cd0ea0e26e0391d157cb5620322d3b20
Ana Isabel Miguel Bello <mbelanabel@gmail.com>
2023-11-01 17:03:32 +0100

    Update 0A0203_uno_parametro.sh

--- 0A0203_uno_parametro.sh
+++ 0A0203_uno_parametro.sh
@@ -3,6 +3,7 @@
 #Objetivo funcional: evaluar si se ha pasado un único parámetro
 #Objetivo didactivo: comparar el numero de parámetros y el numero 1
 #Key words: if-else; $#; 
+
 #!/bin/bash
 if [ $# -eq 1 ]
 then
