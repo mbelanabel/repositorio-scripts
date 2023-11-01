9916bf8ea4f1fce01161e8f9aa66d95d8db2b333
Ana Isabel Miguel Bello <mbelanabel@gmail.com>
2023-11-01 17:03:54 +0100

    Update 0A0205_bucle_sleep.sh

--- 0A0205_bucle_sleep.sh
+++ 0A0205_bucle_sleep.sh
@@ -3,6 +3,7 @@
 #Objetivo funcional: cuando se ejecuta el script este consulta cada 30 segundos si el usuario pasado por parametro ha iniciado sesión en el sistema, si es así, nos avisa.
 #Objetivo didactico: introducir el bucle until-do-done
 #Key words: until-do-done; comillas inversas; -gt; parametros
+
 #!/bin/bash
 until [ `who -a | grep $1 | wc -l ` -gt "0" ]
 do
